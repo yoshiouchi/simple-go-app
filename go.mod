d762b0f262d21d4913357d3440a19a16fec88129
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2024-12-03 10:58:48 +1100

    Arbitrary commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module simple-go-app
 
-go 1.23.0
+go 1.20
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// handler to serve the HTML page
+func handler(w http.ResponseWriter, r *http.Request) {
+	html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Hello, Dog!</title>
+	</head>
+	<body>
+		<h1>Hello, World!</h1>
+		<img src="/dog.jpg" alt="A cute dog" width="300">
+	</body>
+	</html>
+	`
+	fmt.Fprint(w, html)
+}
+
+// handler to serve the dog image
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "dog.jpg")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/dog.jpg", imageHandler)
+	http.ListenAndServe(":8080", nil)
 }
